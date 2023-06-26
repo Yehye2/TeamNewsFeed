a569466c1a1ede4b4e16de61f138a6a2369a3845
Yehye2 <133616786+Yehye2@users.noreply.github.com>
2023-06-26 16:09:59 +0900

    기초 추가 공사

--- migrations/20230626063615-create-posts.js
+++ migrations/20230626063615-create-posts.js
@@ -11,7 +11,12 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: false,
+        references: {
+          model: 'Users', // Users 모델을 참조합니다.
+          key: 'userId', // Users 모델의 userId를 참조합니다.
+          },
+          onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면, Posts 모델의 데이터가 삭제됩니다.
       },
       nickname: {type: Sequelize.STRING,
         allowNull: false
--- models/posts.js
+++ models/posts.js
@@ -27,7 +27,13 @@ module.exports = (sequelize, DataTypes) => {
       type: Sequelize.INTEGER
     },
     userId: {
-      type: Sequelize.INTEGER
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users', // Users 모델을 참조합니다.
+        key: 'userId', // Users 모델의 userId를 참조합니다.
+        },
+        onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면, Posts 모델의 데이터가 삭제됩니다.
     },
     nickname: {type: Sequelize.STRING,
       allowNull: false
