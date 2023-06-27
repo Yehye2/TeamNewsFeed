cd40a880c479f3a7fd43f41ce86bc76ce68186d0
jegwlee <jegwlee@gmail.com>
2023-06-27 19:04:44 +0900

    Modify: description 데이터 타입 수정

--- models/users.js
+++ models/users.js
@@ -42,7 +42,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false
       },
       description: {
-        type: DataTypes.STRING
+        type: DataTypes.TEXT
       },
       profileImage: {
         type: DataTypes.STRING
