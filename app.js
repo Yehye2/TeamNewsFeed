af0a0b1f4686cb33719cc5ab7962ad16551f6c87
jegwlee <jegwlee@gmail.com>
2023-06-27 17:37:37 +0900

    Modify: 라우트 경로 수정

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // 요청의 데이터를 JSON 형식으로 파싱하기 위한 미들웨어 설정
 app.use(cookieParser()); // 쿠키 파싱을 위한 미들웨어 설정
-app.use("/", [usersRouter, profileRouter, authRouter, postRouter]);
+app.use("/api", [usersRouter, profileRouter, authRouter, postRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트 번호로 서버가 실행되었습니다.");
