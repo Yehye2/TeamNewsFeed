de5306381b3bc1ed53231cd01d01a03e9e37c271
jegwlee <jegwlee@gmail.com>
2023-06-27 16:56:01 +0900

    Feat: 게시글 수정 기능 구현

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const { Posts } = require('../models');
 
-
-router.post("/posts", create);
-router.get("/posts", getAll);
-router.get("/posts:postId", getOne);
-
+router.post('/posts', create);
+router.get('/posts', getAll);
+router.get('/posts:postId', getOne);
+router.put('/posts:postId', update);
 
 async function create(req, res) {
   try {
@@ -15,7 +14,7 @@ async function create(req, res) {
     const post = new Posts({ UserId, nickname, title, content });
 
     if (!title || !content) {
-      return res.status(412).json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
+      return res.status(412).json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
     }
 
     await post.save();
@@ -25,21 +24,21 @@ async function create(req, res) {
   } catch (error) {
     console.error(`Error: ${error.message}`);
     return res.status(400).json({
-      message: "게시글 작성에 실패했습니다."
+      message: '게시글 작성에 실패했습니다.',
     });
   }
 }
 async function getAll(req, res) {
   try {
     const posts = await Posts.findAll({
-      attributes: ["postId", "UserId", "nickname", "title", "createdAt", "updatedAt"],
-      order: [["createdAt", "DESC"]]
+      attributes: ['postId', 'UserId', 'nickname', 'title', 'createdAt', 'updatedAt'],
+      order: [['createdAt', 'DESC']],
     });
     res.status(200).json({ posts });
   } catch (error) {
     console.error(`Error: ${error.message}`);
     res.status(400).json({
-      errorMessage: "게시글 조회에 실패했습니다."
+      errorMessage: '게시글 조회에 실패했습니다.',
     });
   }
 }
@@ -54,16 +53,41 @@ async function getOne(req, res) {
 
     if (!post) {
       return res.status(400).json({ errorMessage: '게시글이 존재하지 않습니다.' });
-    } 
+    }
 
     res.status(200).json({ post });
-    
   } catch (error) {
     console.error(`Error: ${error.message}`);
     res.status(400).json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
   }
 }
 
+async function update(req, res) {
+  try {
+    const { postId } = req.params;
+    const { title, content } = req.body;
+    const post = await Posts.findOne({ where: { postId } });
 
+    if (!title || !content) {
+      res.status(412).json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
+      return;
+    }
+
+    if (!post) {
+      res.status(404).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
+    } else {
+      try {
+        await Posts.update({ title, content }, { where: { postId } });
+        // getAll API 호출
+        getAll(req, res);
+      } catch (error) {
+        res.status(401).json({ errorMessage: '게시글이 정상적으로 수정되지 않았습니다.' });
+      }
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    return res.status(400).json({ errorMessage: '게시글 수정에 실패했습니다.' });
+  }
+}
 
 module.exports = router;
