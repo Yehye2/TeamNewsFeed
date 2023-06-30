1dcb70005c3d3d405bb001f4f3f10ae512241ae1
Yehye2 <133616786+Yehye2@users.noreply.github.com>
2023-06-30 09:16:05 +0900

    Revert "Merge remote-tracking branch 'origin/TY_mainpage'"
    This reverts commit e72661af715c7e39acb3d6a4faf2ffae59c28df8, reversing
    changes made to e8312de128990df2b4783d55acaa8bb4d3b5909f.

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get("/bestsellers", (req, res) => {
       params: {
         ttbkey: "ttbtiwh11427001",
         QueryType: "BestSeller",
-        MaxResults: 10,
+        MaxResults: 5,
         start: 1,
         SearchTarget: "Book",
         output: "js",
